6c1eb892876f8128e6c0eb7813e6b9db604dac61
codeby-rafay <rafayali146@gmail.com>
2026-07-10 15:53:51 +0500

    user profile route created

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const { authMiddleware } = require("../../middleware/auth.middleware");
+const profileModel = require("../../models/Profile");
+const userModel = require("../../models/User");
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Profile Route"));
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  Private
+router.get("/me", authMiddleware, async (req, res) => {
+  try {
+    const profile = await profileModel
+      .findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.status(200).json({ msg: "Profile fetched successfully", profile });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
 
 module.exports = router;
