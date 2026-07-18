711acfb49e41a79e90abf70e12000e65af67fd87
codeby-rafay <rafayali146@gmail.com>
2026-07-18 19:30:29 +0500

    Alerts created using Redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,34 @@
-import { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
+    <Provider store={store}>
+      <Router>
         <Navbar />
+        
+        {/* Landing page - full width */}
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+
+        {/* Other pages - inside container */}
         <section className="container">
+          <Alert />
           <Routes>
-            <Route path="/" element={<Landing />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
           </Routes>
         </section>
-      </Fragment>
-    </Router>
+      </Router>
+    </Provider>
   );
 };
 
--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert.action";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,7 +21,7 @@ const Register = () => {
     e.preventDefault();
 
     if (password !== password2) {
-      console.log("Passwords do not match");
+      setAlert("Passwords do not match", "danger");
     } else {
       console.log(formData);
 
@@ -98,4 +101,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
