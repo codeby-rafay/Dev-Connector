77e0d7689ae6b0bee947d38383e183761522bcd3
codeby-rafay <rafayali146@gmail.com>
2026-07-19 20:35:39 +0500

    User Loaded created using redux

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const connectDB = require("./config/db");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
 connectDB();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 const PORT = process.env.PORT || 5000;
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import setAuthToken from "./utils/setAuthtoken";
 
-import { Provider } from "react-redux";
 import store from "./store";
+import { Provider } from "react-redux";
+import { loadUser } from "./actions/auth.action";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Navbar />
-        
+
         {/* Landing page - full width */}
         <Routes>
           <Route path="/" element={<Landing />} />
